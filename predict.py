271a8a2024379c46ebecc4cb224e305edd4ba5bb
Garima Bisht <bisht.gar14@gmail.com>
2025-07-03 23:10:25 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,16 +1,16 @@
 print("Script started!")
 from ultralytics import YOLO
 
-# Path to your trained model weights
+# Path to trained model weights
 model_path = "C:\\Users\\Garima Bisht\\OneDrive\\Desktop\\The_Litter_Coach\\runs\\detect\\litter_coach_detection_v13\\weights\\best.pt"
 
-# Path to your images for inference (can be a single image or a folder)
+# Path to images for inference (can be a single image or a folder)
 source_path = "C:\\Users\\Garima Bisht\\OneDrive\\Desktop\\The_Litter_Coach\\test_inference_images\\"
 
 # Load the trained model
 model = YOLO(model_path)
 
-# Perform inference
+
 results = model.predict(source=source_path, save=True, conf=0.25)
 
 print("Inference completed! Check the 'runs/detect/predict/' folder for results.")
